84a37e48e4fe872b7a89a6a25d47e5aba5de9958
gabrimarch3 <gabrimarche@protonmail.com>
2023-12-21 14:19:00 +0100

    fixing vercel connection 11

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -8,17 +8,22 @@ export async function POST(req) {
     console.log("Request Body:", body);
     const ticketData = body.formData;
     await Ticket.create(ticketData);
+    console.log("Ticket Created");
     return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
   } catch (error) {
+    console.error("Error creating ticket:", error);
     return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
 }
 
 export async function GET() {
   try {
+    console.log("GET RUN");
     const tickets = await Ticket.find();
+    console.log("Tickets Found:", tickets);
     return NextResponse.json({ tickets }, { status: 200 });
   } catch (error) {
+    console.error("Error retrieving tickets:", error);
     return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
 }
